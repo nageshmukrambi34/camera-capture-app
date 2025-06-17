b25a7b0fad1f4a47b0e85628203198e708d28793
nageshmukrambi34 <126222419+nageshmukrambi34@users.noreply.github.com>
2025-06-17 19:18:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const storage = multer.diskStorage({
   },
 });
 
-const upload = multer({ storage });
+const upload = multer({ delt:'upload/' });
 
 app.use(express.static("uploads")); // Serves uploaded images
 
