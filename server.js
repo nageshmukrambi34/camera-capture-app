fbe41b0c9971b6cefc3d567609f233de205a8985
nageshmukrambi34 <126222419+nageshmukrambi34@users.noreply.github.com>
2025-06-17 18:05:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -28,5 +28,5 @@ app.post('/upload', upload.single('image'), (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(Server running on port ${port});
+  console.log(Server running on port ${port}`);
 });
